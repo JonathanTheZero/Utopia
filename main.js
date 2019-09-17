bd09c6a3a7ba4e8573e9fc6d4ebf48716de24df5
Jonathan <jonathan.frohne@web.de>
2019-09-17 21:37:20 +0200

    Added function with express library for future release

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ const config = require("./config.json");
 const fs = require("fs");
 require("./alliances.js")();
 const client = new Discord.Client();
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
