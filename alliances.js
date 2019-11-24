d9d0ee46045d56e32f23c2b2ab8a9a12d644f6b6
Jonathan <jonathan.frohne@web.de>
2019-11-24 09:44:19 +0100

    Revert "So i made personal farms a thing boi"

--- alliances.js
+++ alliances.js
@@ -26,7 +26,7 @@ module.exports = function () {
       },
       money: 0,
       tax: 5,
-      /*population: 0,*/
+      population: 0,
       coLeaders: [],
       members: [],
       upgrades: {
@@ -314,66 +314,6 @@ module.exports = function () {
         else {
           parsedDataAlliances[ind].money -= 5000000;
           parsedDataAlliances[ind].level = 4;
-          fs.writeFileSync("alliances.json", JSON.stringify(parsedDataAlliances, null, 2))
-          fs.writeFileSync("userdata.json", JSON.stringify(parsedData, null, 2))
-          return "Succesfully upgraded your alliance to level 4. Your alliance can now own each farm four times.";
-        }
-      }
-      else if (parsedDataAlliances[ind].level == 4) {
-        if (parsedDataAlliances[ind].money < 10000000) {
-          return "you don't have enough money to upgrade your alliance to level 4. Upgrading your alliance to level 4 costs 5,000,000 coins.";
-        }
-        else {
-          parsedDataAlliances[ind].money -= 10000000;
-          parsedDataAlliances[ind].level = 5;
-          fs.writeFileSync("alliances.json", JSON.stringify(parsedDataAlliances, null, 2))
-          fs.writeFileSync("userdata.json", JSON.stringify(parsedData, null, 2))
-          return "Succesfully upgraded your alliance to level 4. Your alliance can now own each farm four times.";
-        }
-      }
-      else if (parsedDataAlliances[ind].level == 5) {
-        if (parsedDataAlliances[ind].money < 20000000) {
-          return "you don't have enough money to upgrade your alliance to level 4. Upgrading your alliance to level 4 costs 5,000,000 coins.";
-        }
-        else {
-          parsedDataAlliances[ind].money -= 20000000;
-          parsedDataAlliances[ind].level = 6;
-          fs.writeFileSync("alliances.json", JSON.stringify(parsedDataAlliances, null, 2))
-          fs.writeFileSync("userdata.json", JSON.stringify(parsedData, null, 2))
-          return "Succesfully upgraded your alliance to level 4. Your alliance can now own each farm four times.";
-        }
-      }
-      else if (parsedDataAlliances[ind].level == 6) {
-        if (parsedDataAlliances[ind].money < 50000000) {
-          return "you don't have enough money to upgrade your alliance to level 4. Upgrading your alliance to level 4 costs 5,000,000 coins.";
-        }
-        else {
-          parsedDataAlliances[ind].money -= 50000000;
-          parsedDataAlliances[ind].level = 7;
-          fs.writeFileSync("alliances.json", JSON.stringify(parsedDataAlliances, null, 2))
-          fs.writeFileSync("userdata.json", JSON.stringify(parsedData, null, 2))
-          return "Succesfully upgraded your alliance to level 4. Your alliance can now own each farm four times.";
-        }
-      }
-      else if (parsedDataAlliances[ind].level == 8) {
-        if (parsedDataAlliances[ind].money < 100000000) {
-          return "you don't have enough money to upgrade your alliance to level 4. Upgrading your alliance to level 4 costs 5,000,000 coins.";
-        }
-        else {
-          parsedDataAlliances[ind].money -= 100000000;
-          parsedDataAlliances[ind].level = 9;
-          fs.writeFileSync("alliances.json", JSON.stringify(parsedDataAlliances, null, 2))
-          fs.writeFileSync("userdata.json", JSON.stringify(parsedData, null, 2))
-          return "Succesfully upgraded your alliance to level 4. Your alliance can now own each farm four times.";
-        }
-      }
-      else if (parsedDataAlliances[ind].level == 9) {
-        if (parsedDataAlliances[ind].money < 500000000) {
-          return "you don't have enough money to upgrade your alliance to level 4. Upgrading your alliance to level 4 costs 5,000,000 coins.";
-        }
-        else {
-          parsedDataAlliances[ind].money -= 500000000;
-          parsedDataAlliances[ind].level = 10;
           fs.writeFileSync("alliances.json", JSON.stringify(parsedDataAlliances, null, 2))
           fs.writeFileSync("userdata.json", JSON.stringify(parsedData, null, 2))
           return "Succesfully upgraded your alliance to level 4. Your alliance can now own each farm four times.";
