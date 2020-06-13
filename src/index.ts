2da777ece537cca1b2692478d695e507fb08465d
Sam <qureshisamin25@gmail.com>
2020-06-13 14:47:47 -0400

    Removed modmail file
    removed the file, and added the code into the main index.ts file

--- src/index.ts
+++ src/index.ts
@@ -229,7 +229,57 @@ client.on("message", async message => {
     }
 
     else if(command === "mail" || command === "anonmail"){
-        modmail(command, args, client, message)
+        if(!args) return message.reply("The command is .mail <suggestion|mods> `message`");
+
+        if (command === "mail"){
+            if(["s"].includes(args[0])){
+                let channel = <Discord.TextChannel>client.channels.get("621046082859958275");
+                channel.send({
+                    embed: {
+                        title: `New Suggestion from ${message.author.username}`,
+                        description: `${message.author.id}\n${args.slice(1).join(" ")}`,
+                        color: 0xFF0000
+                    }
+                })
+
+            }
+
+            else if(["m"]){
+                let channel = <Discord.TextChannel>client.channels.get("721062176042778666");
+                channel.send({
+                    embed: {
+                        title: `New Message from ${message.author.username}`,
+                        description: `${message.author.id}\n${args.slice(1).join(" ")}`,
+                        color: 0xFF0000
+                    }
+                })
+            }
+        }
+
+        else if (command === "anonmail"){
+            if(["s"].includes(args[0])){
+                let channel = <Discord.TextChannel>client.channels.get("621046082859958275");
+                channel.send({
+                    embed: {
+                        title: `New Suggestion`,
+                        description: `${args.slice(1).join(" ")}`,
+                        color: 0xFF0000
+                    }
+                })
+
+            }
+
+            else if(["m"]){
+                let channel = <Discord.TextChannel>client.channels.get("721062176042778666");
+                channel.send({
+                    embed: {
+                        title: `New Message`,
+                        description: `${args.slice(1).join(" ")}`,
+                        color: 0xFF0000
+                    }
+                })
+            }
+        }
     }
 
     else if (command === "testmsg"){
